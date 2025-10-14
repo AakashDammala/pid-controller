326ad26d65efcafe35a2efb6e8d5c8bae7271fd4
Siddhant <iamsid@umd.edu>
2025-10-14 02:37:11 -0400

    This commit updates the formatting of code to follow Google Style guidelines. It also does: 1) change pragma once to ifndef to follow guideline 2) Adds cppcheck output

--- libs/libpid/libpid.hpp
+++ libs/libpid/libpid.hpp
@@ -1,20 +1,27 @@
 /**
  * @file libpid.hpp
- * @author Aakash Shetty Dammala (asd@umd.edu)
+ * @author Aakash Shetty Dammala (asd@umd.edu) - Part 1 (Driver)
+ * @author Tirth Sadaria (tirths@umd.edu) - Part 1 (Navigator)
+ * @author Siddhant Deshmukh (siddhantd@umd.edu) - Part 2 (Driver)
+ * @author Grayson Gilbert (ggilbert@umd.edu) - Part 2 (Navigator)
+
  * @brief Simple PID controller interface.
  * @version 0.1
  * @date 2025-10-07
+ * @version 0.2
+ * @date 2025-10-14
  * @copyright Copyright (c) 2025
  */
 
-#pragma once
+#ifndef LIBPID_HPP_
+#define LIBPID_HPP_
 
 /**
  * @class PIDController
  * @brief Minimal PID controller for scalar inputs.
  */
 class PIDController {
-public:
+ public:
   /**
    * @brief Construct the PID controller.
    * @param Kp Proportional gain.
@@ -41,34 +48,36 @@ public:
    */
   double compute(double target_setpoint, double measured_value);
 
-private:
-  const double Kp_; // proportional gain
-  const double Ki_; // integral gain
-  const double Kd_; // differential gain
-  const double delta_time_; // loop time of the controller
-  const double min_output_; // minimum output provided by the controller
-  const double max_output_; // maximum output provided by the controller 
-  double prev_error_; // previous error value
-  double integral_error_; // cumulative integral error
+ private:
+  const double Kp_;          // proportional gain
+  const double Ki_;          // integral gain
+  const double Kd_;          // differential gain
+  const double delta_time_;  // loop time of the controller
+  const double min_output_;  // minimum output provided by the controller
+  const double max_output_;  // maximum output provided by the controller
+  double prev_error_;        // previous error value
+  double integral_error_;    // cumulative integral error
   /**
    * @brief Compute the proportional term
-   * 
+   *
    * @param error Error value
    * @return double Controller proportional output
    */
-  double compute_propotional(double error); // Compute proportional term
+  double compute_propotional(double error);  // Compute proportional term
   /**
    * @brief Compute the derivative term
-   * 
+   *
    * @param error Error value
    * @return double Controller derivative output
    */
-  double compute_derivative(double error); // Compute derivative term
+  double compute_derivative(double error);  // Compute derivative term
   /**
    * @brief Compute the integral term
-   * 
+   *
    * @param error Error value
    * @return double Controller integral output
    */
-  double compute_integral(double error); // Compute integral term
+  double compute_integral(double error);  // Compute integral term
 };
+
+#endif  // LIBPID_HPP_
